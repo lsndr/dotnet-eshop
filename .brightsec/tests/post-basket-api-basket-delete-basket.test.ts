38756f20abdc428300c60ec8cd85014be686b194
Alexander <lsndrme@ya.ru>
2026-06-23 19:30:10 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/post-basket-api-basket-delete-basket.test.ts
@@ -0,0 +1,45 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /BasketApi.Basket/DeleteBasket', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['jwt', 'id_enumeration'],
+      attackParamLocations: [AttackParamLocation.HEADER, AttackParamLocation.BODY],
+      starMetadata: {
+        code_source: 'lsndr/dotnet-eshop:main',
+        databases: ['PostgreSQL'],
+        user_roles: []
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/BasketApi.Basket/DeleteBasket`,
+      body: 'AAAAAAA=',
+      headers: {
+        authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.example.signature',
+        'content-type': 'application/grpc',
+        te: 'trailers'
+      }
+    });
+});
